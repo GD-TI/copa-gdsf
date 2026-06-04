9ab6f912d625171e8a4ab03c861bb347b3204372
aleneresss <alessandroneres166@gmail.com>
2026-06-04 15:54:20 -0300

    data pagamento

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def build_i_param(start_date: str, end_date: str) -> str:
         "banco": [], "not_banco": [],
         "promotora": [], "not_promotora": [],
         "status": [], "not_status": [],
-        
+
         "produto": ["7","13"], "not_produto": [],
         "convenio": [], "not_convenio": [],
         "equipe": [], "not_equipe": [],
@@ -94,7 +94,7 @@ def build_i_param(start_date: str, end_date: str) -> str:
         "hideDuplicadas": False,
         "hide_repassado": False,
         "data": {
-            "tipo": "cadastro",
+            "tipo": "pagamento",
             "startDate": start_date,
             "endDate": end_date,
             "intervalo": intervalo,
